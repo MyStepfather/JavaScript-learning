5716cd47c4dff363b0d6f5c6a9e093a9d512e791
MyStepfather <122305776+MyStepfather@users.noreply.github.com>
2023-03-07 01:19:39 +0300

    06/03 - learning function

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money, time;
+
+function start() {
+	money = +prompt("Ваш бюджет на месяц?", '');
 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+	while(isNaN(money) || money == "" || money == null) {
+		money = +prompt("Ваш бюджет на месяц?", '');
+	}
+}
+
+start();
+
 
 let appData = {
 	budget: money,
@@ -7,15 +18,72 @@ let appData = {
 	optionalExpenses: {},
 	income: [],
 	timeData: time,
-	savings: false
+	savings: true
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
+function chooseExpenses() {
+	for (let i = 0; i < 2; i++) {
+		let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+			b = prompt("Во сколько обойдется?", '');
+	
+		if ((typeof(a))=== 'string' && (typeof(a)) !=null && (typeof(b)) !=null
+			&& a != '' && b != '' && a.length < 50) {
+			console.log("Done");
+			appData.expenses[a] = b;
+		} else {
+			alert("Некорректные данные, введите еще раз.");
+			i--;
+		}	
+	};
+}
+chooseExpenses();
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+// - метод округления до целого числа, в скобках можно указать, сколько цифер после запятой остваить
 
-alert(appData.budget / 30);
+
+
+
+
+function checkSavings() {
+	if (appData.savings == true) {
+		let save = +prompt("Какова сумма накоплений?"),
+			percent = +prompt("Под какой процент?");
+
+		appData.monthIncome = save/100/12*percent;
+		alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+	}
+}
+
+checkSavings();
+
+function detectDayBudget() {
+	appData.moneyPerDay = (appData.budget / 30).toFixed();
+	alert("Ежедневый бюджет: " + appData.moneyPerDay);
+}
+
+detectDayBudget();
+
+function detectLevel() {
+	if(appData.moneyPerDay < 100) {
+		console.log("Минимальный уровень достатка");
+	} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+		console.log("Средний уровень достатка");
+	} else if (appData.moneyPerDay > 2000) {
+		console.log("Высокий уровень достатка");
+	} else {
+		console.log("Произошла ошибка");
+	}
+}
+
+detectLevel();
+
+function chooseOptExpenses() {
+	let a = 1;
+	for (let i = 0; i<3; i++) {
+		let b = prompt("Статья необязательных расходов?");
+		appData.optionalExpenses[a] = b;
+		a++;
+	}
+}
+
+chooseOptExpenses();
